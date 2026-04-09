a0ee131a86a1b5667289a3b62bbaa798ad1e8976
Atharv3527 <atharvwaykar3@gmail.com>
2026-04-09 22:21:34 +0530

    final

--- src/components/ParticleDivider.jsx
+++ src/components/ParticleDivider.jsx
@@ -4,16 +4,7 @@ import './ParticleDivider.css';
 export const ParticleDivider = () => {
   return (
     <div className="particle-divider-container">
-      <div className="divider-line">
-        <div className="divider-pulse"></div>
-        <div className="divider-glow-left"></div>
-        <div className="divider-glow-right"></div>
-      </div>
-      <div className="divider-orbs">
-        <div className="orb orb-1"></div>
-        <div className="orb orb-2"></div>
-        <div className="orb orb-3"></div>
-      </div>
+      <div className="divider-line" />
     </div>
   );
 };
